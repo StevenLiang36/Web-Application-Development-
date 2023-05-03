2e0dd6155960922497b611606b65541c997f313f
StevenLiang36 <112482931+StevenLiang36@users.noreply.github.com>
2023-05-03 21:32:41 +0800

    Add files via upload

---
+++ chatBox/server.js
@@ -0,0 +1,87 @@
+// const app = require('express')();
+// const bodyParser = require('body-parser');
+//
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
+//
+// // const app = express();
+// const port = 3000;
+//
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+//
+// app.post('/chatBox', (req, res) => {
+//     let userName = req.body.userName;
+//     res.send('Hello! ' + userName);
+// });
+//
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+//     socket.on('disconnect', () => {
+//         console.log('A user disconnected');
+//     });
+// });
+//
+// app.listen(port, () => {
+// console.log(`Example app listening on port ${port}`)
+// });
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// 如何实现：跳转到chatBox后，hello后可以接收到用户在index页输入的名字？
+// 按道理来说是要用EJS的，因为html需要接受传过来的用户名
+// 没错！就是用EJS!!!
+
+app.post('/chatBox', (req, res) => {
+    let userName = req.body.userName;
+    // res.send('Hello! ' + userName);
+    // res.sendFile(__dirname + '/chatBox.ejs');
+    res.render('chatBox', { userName: userName });
+});
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('clicked', (msg) => {
+        io.emit('msgReceived',msg);
+    })
+});
+
+// chatGPT的socket版本：
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+//     socket.on('chat message', (msg) => {
+//         console.log('message: ' + msg);
+//         io.emit('chat message', msg);
+//     });
+//     socket.on('disconnect', () => {
+//         console.log('A user disconnected');
+//     });
+// });
+
+http.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
